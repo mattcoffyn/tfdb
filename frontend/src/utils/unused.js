0f8616e7e9a9e4d09adb9c2cbe7675e550bed5a6
Matt Coffyn <matt.coffyn@gmail.com>
2021-03-18 09:44:26 +0000

    cleaned up unused files

--- frontend/src/utils/unused.js
+++
@@ -1,8 +0,0 @@
-// ** Merges two queries into one array and sorts all by updatedAt date/time
-// function mergeDataSources(a, b) {
-//   const newA = a.concat(b);
-//   newA.sort((a, b) =>
-//     a._updatedAt < b._updatedAt ? 1 : b._updatedAt < a._updatedAt ? -1 : 0
-//   );
-//   return newA;
-// }
